59ffeb327d4db97bd881dbb1a6e14c2028764716
ppp-pp-pp <willydoo@mail.com>
2020-12-07 11:25:44 +0700

    Update Minggu 11

--- fp.js
+++ fp.js
@@ -1,13 +1,13 @@
-var scene = new THREE.Scene();
+let scene = new THREE.Scene();
 
-var w = window.innerWidth;
-var h = window.innerHeight;
-var camera = new THREE.PerspectiveCamera(35, w/h, 1, 100);
+let w = window.innerWidth;
+let h = window.innerHeight;
+let camera = new THREE.PerspectiveCamera(35, w/h, 1, 100);
 camera.position.x = 200;
 camera.position.y = 5;
 camera.position.z = 240;
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
 renderer.setClearColor(0xffffff);
 renderer.shadowMap.enabled = true;
@@ -18,7 +18,7 @@ controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(200, 0, 200);
 camera.lookAt(new THREE.Vector3(200, 0, 200));
 
-var light = new THREE.DirectionalLight(0xffffff, 1, 1000);
+let light = new THREE.DirectionalLight(0xffffff, 1, 1000);
 light.castShadow = true;
 light.position.set(20, 20, 20);
 scene.add(light);
@@ -28,18 +28,11 @@ light.shadowCameraRight = 20;
 light.shadowCameraTop = 20;
 light.shadowCameraBottom = -20;
 
-// var geometry = new THREE.PlaneGeometry(2000, 2000);
-// var material = new THREE.MeshPhongMaterial({color: 0xffffff, side: THREE.DoubleSide});
-// var ground = new THREE.Mesh(geometry, material);
-// ground.rotateX(-Math.PI/2);
-// ground.receiveShadow = true;
-// scene.add(ground);
-
-var loader = new THREE.GLTFLoader();
-var level1Array = new Array();
+let loader = new THREE.GLTFLoader();
+let level1Array = new Array();
 loader.load('models/pipe.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, 0, 200);
         scene.add(model);
         level1Array.push(model);
@@ -48,7 +41,7 @@ loader.load('models/pipe.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, 5, 195);
         scene.add(model);
         level1Array.push(model);
@@ -57,7 +50,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(205, 5, 200);
         scene.add(model);
         level1Array.push(model);
@@ -66,7 +59,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, 5, 205);
         scene.add(model);
         level1Array.push(model);
@@ -75,7 +68,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(195, 0, 195);
         scene.add(model);
         level1Array.push(model);
@@ -84,7 +77,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(195, 0, 205);
         scene.add(model);
         level1Array.push(model);
@@ -93,7 +86,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, -5, 195);
         scene.add(model);
         level1Array.push(model);
@@ -102,7 +95,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(205, -5, 200);
         scene.add(model);
         level1Array.push(model);
@@ -111,7 +104,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, -5, 205);
         scene.add(model);
         level1Array.push(model);
@@ -120,7 +113,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, 5, 200);
         scene.add(model);
         level1Array.push(model);
@@ -129,7 +122,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, 0, 195);
         scene.add(model);
         level1Array.push(model);
@@ -138,7 +131,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(195, 0, 200);
         scene.add(model);
         level1Array.push(model);
@@ -147,7 +140,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(205, 0, 200);
         scene.add(model);
         level1Array.push(model);
@@ -156,7 +149,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, 0, 205);
         scene.add(model);
         level1Array.push(model);
@@ -165,7 +158,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(200, -5, 200);
         scene.add(model);
         level1Array.push(model);
@@ -174,10 +167,10 @@ loader.load('models/pipe32.glb',
 
 //Level 2
 
-var level2Array = new Array();
+let level2Array = new Array();
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, 5, -5);
         scene.add(model);
         level2Array.push(model);
@@ -186,7 +179,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, 5, -5);
         scene.add(model);
         level2Array.push(model);
@@ -195,7 +188,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, 5, -5);
         scene.add(model);
         level2Array.push(model);
@@ -204,7 +197,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, 5, 0);
         scene.add(model);
         level2Array.push(model);
@@ -213,7 +206,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, 5, 0);
         scene.add(model);
         level2Array.push(model);
@@ -222,7 +215,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, 5, 0);
         scene.add(model);
         level2Array.push(model);
@@ -231,7 +224,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, 5, 5);
         scene.add(model);
         level2Array.push(model);
@@ -240,7 +233,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, 5, 5);
         scene.add(model);
         level2Array.push(model);
@@ -249,7 +242,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, 5, 5);
         scene.add(model);
         level2Array.push(model);
@@ -258,7 +251,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, 0, -5);
         scene.add(model);
         level2Array.push(model);
@@ -267,7 +260,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe4.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, 0, -5);
         scene.add(model);
         level2Array.push(model);
@@ -276,7 +269,7 @@ loader.load('models/pipe4.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, 0, -5);
         scene.add(model);
         level2Array.push(model);
@@ -285,7 +278,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, 0, 0);
         scene.add(model);
         level2Array.push(model);
@@ -294,7 +287,7 @@ loader.load('models/pipe.glb',
 
 loader.load('models/pipe.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, 0, 0);
         scene.add(model);
         level2Array.push(model);
@@ -303,7 +296,7 @@ loader.load('models/pipe.glb',
 
 loader.load('models/pipe.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, 0, 0);
         scene.add(model);
         level2Array.push(model);
@@ -312,7 +305,7 @@ loader.load('models/pipe.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, 0, 5);
         scene.add(model);
         level2Array.push(model);
@@ -321,7 +314,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe4.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, 0, 5);
         scene.add(model);
         level2Array.push(model);
@@ -330,7 +323,7 @@ loader.load('models/pipe4.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, 0, 5);
         scene.add(model);
         level2Array.push(model);
@@ -339,7 +332,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, -5, -5);
         scene.add(model);
         level2Array.push(model);
@@ -348,7 +341,7 @@ loader.load('models/pipe2.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, -5, -5);
         scene.add(model);
         level2Array.push(model);
@@ -357,7 +350,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, -5, -5);
         scene.add(model);
         level2Array.push(model);
@@ -366,7 +359,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, -5, 0);
         scene.add(model);
         level2Array.push(model);
@@ -375,7 +368,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, -5, 0);
         scene.add(model);
         level2Array.push(model);
@@ -384,7 +377,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, -5, 0);
         scene.add(model);
         level2Array.push(model);
@@ -393,7 +386,7 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe33.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(-5, -5, 5);
         scene.add(model);
         level2Array.push(model);
@@ -402,7 +395,7 @@ loader.load('models/pipe33.glb',
 
 loader.load('models/pipe32.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(0, -5, 5);
         scene.add(model);
         level2Array.push(model);
@@ -411,16 +404,24 @@ loader.load('models/pipe32.glb',
 
 loader.load('models/pipe2.glb',
     function(gltf) {
-        var model = gltf.scene;
+        let model = gltf.scene;
         model.position.set(5, -5, 5);
         scene.add(model);
         level2Array.push(model);
     }
 )
 
+let axis1 = new THREE.AxesHelper(20);
+let axis2 = new THREE.AxesHelper(20);
+axis1.name = 'axis'
+axis2.name = 'axis'
+axis2.position.set(200, 0, 200);
+scene.add(axis1);
+scene.add(axis2);
+
 // loader.load('models/pipe.glb',
 // 	function(gltf) {
-//         var model = gltf.scene;
+//         let model = gltf.scene;
 //         // model.traverse((o) => {
 //         //     if (o.isMesh) o.material = new THREE.MeshPhongMaterial();
 //         // });
@@ -429,45 +430,54 @@ loader.load('models/pipe2.glb',
 // 	}
 // )
 
-// var isSolved = 0;
-// function solve() {
-//     var pi = Math.PI;
-//     level1Array[1].rotation.y = -pi/2;
-//     level1Array[2].rotation.x = -pi/2;
-//     level1Array[2].rotation.y = pi/2;
-//     level1Array[3].rotation.y = -pi/2;
-//     level1Array[4].rotation.x = -pi/2;
-//     level1Array[4].rotation.y = pi/2;
-//     level1Array[5].rotation.x = -pi/2;
-//     level1Array[5].rotation.y = pi/2;
-//     level1Array[6].rotation.y = -pi/2;
-//     level1Array[7].rotation.z = -pi/2;
-//     level1Array[8].rotation.x = pi/2;
-//     level1Array[8].rotation.y = -pi/2;
-//     level1Array[9].rotation.x = pi/2;
-//     level1Array[9].rotation.z = -pi/2;
-//     level1Array[11].rotation.x = pi;
-//     level1Array[12].rotation.z = pi/2;
-//     level1Array[13].rotation.x = -pi/2;
-//     level1Array[13].rotation.y = -pi/2;
-//     level1Array[14].rotation.x = -pi/2;
-//     level1Array[14].rotation.z = pi/2;
-// }
-
-var mouse = new THREE.Vector2;
-var raycaster = new THREE.Raycaster();
-var intersected;
+let mouse = new THREE.Vector2;
+let raycaster = new THREE.Raycaster();
+let intersected;
 
 document.addEventListener("mousemove", event => {
   	mouse.x = (event.clientX/w)*2-1;
 	mouse.y = -(event.clientY/h)*2+1;
 });
 
-var rotationMode = 0;
+let rotationMode = 0;
+let parameters = {
+    a: 'X',
+    b: true
+}
+
+let gui = new dat.GUI();
+function displayGUI(){
+    let val;
+
+    let modeChange = gui.add(parameters, 'a', ['X', 'Y', 'Z']).name('Rotation Mode');
+    modeChange.onChange(function(val){
+        if (val == 'X') rotationMode = 0;
+        if (val == 'Y') rotationMode = 1;
+        if (val == 'Z') rotationMode = 2;
+    });
+
+    let axisVisibleChange = gui.add(parameters, 'b').name('Show Axis');
+    axisVisibleChange.onChange(function(val){
+        axis1.visible = val;
+        axis2.visible = val;
+    });
+
+    gui.open();
+}
+
 document.addEventListener("keydown", event => {
-    if (event.key == 'X' || event.key == 'x') rotationMode = 0;
-    if (event.key == 'Y' || event.key == 'y') rotationMode = 1;
-    if (event.key == 'Z' || event.key == 'z') rotationMode = 2;
+    if (event.key == 'X' || event.key == 'x'){
+        rotationMode = 0;
+        parameters['a'] = 'X';
+    }
+    if (event.key == 'Y' || event.key == 'y'){
+        rotationMode = 1;
+        parameters['a'] = 'Y';
+    }
+    if (event.key == 'Z' || event.key == 'z'){
+        rotationMode = 2;
+        parameters['a'] = 'Z';
+    }
     if (event.key == '1') {
         camera.position.x = 200;
         camera.position.y = 5;
@@ -484,28 +494,33 @@ document.addEventListener("keydown", event => {
     }
 });
 
-function updateMode(){
-    if(rotationMode == 0) document.getElementById("rotationMode").innerHTML = "Rotation Mode: X";
-    if(rotationMode == 1) document.getElementById("rotationMode").innerHTML = "Rotation Mode: Y";
-    if(rotationMode == 2) document.getElementById("rotationMode").innerHTML = "Rotation Mode: Z";
+function updateGUI(){
+    for (let i in gui.__controllers){
+        gui.__controllers[i].updateDisplay();
+    }
 }
 
+let xvec = new THREE.Vector3(1, 0, 0);
+let yvec = new THREE.Vector3(0, 1, 0);
+let zvec = new THREE.Vector3(0, 0, 1);
 document.addEventListener("click", function() {
 	raycaster.setFromCamera(mouse, camera);
-	var intersects = raycaster.intersectObjects(scene.children, true);
+	let intersects = raycaster.intersectObjects(scene.children, true);
 	if (intersects.length){
-        console.log(intersects[0]);
-        var pi = Math.PI;
-        if(rotationMode == 0) intersects[0].object.rotation.x += pi/2;
-        if(rotationMode == 1) intersects[0].object.rotation.y += pi/2;
-        if(rotationMode == 2) intersects[0].object.rotation.z += pi/2;
+        if(intersects[0].object.name != 'axis'){
+            console.log(intersects[0]);
+            let pi = Math.PI;
+            if(rotationMode == 0) intersects[0].object.rotateOnWorldAxis(xvec, pi/2);
+            if(rotationMode == 1) intersects[0].object.rotateOnWorldAxis(yvec, pi/2);
+            if(rotationMode == 2) intersects[0].object.rotateOnWorldAxis(zvec, pi/2);
+        }
 	}
 });
 
-var r = 191, g = 68, b = 92;
-var rr = 0, rg = 0, rb = 0;
+let r = 191, g = 68, b = 92;
+let rr = 0, rg = 0, rb = 0;
 function animateBG() {
-    var rand = Math.floor(Math.random() * 3);
+    let rand = Math.floor(Math.random() * 3);
     if (rand == 0 && rr == 0) r = (r + 1) % 256;
     if (rand == 1 && rg == 0) g = (g + 1) % 256;
     if (rand == 2 && rb == 0) b = (b + 1) % 256;
@@ -518,20 +533,16 @@ function animateBG() {
     if (g == 50) rg = 0;
     if (b == 255) rb = 1;
     if (b == 50) rb = 0;
-    var colorStr = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    var col = new THREE.Color(colorStr);
-    renderer.setClearColor(col);
+    let colorStr = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    renderer.setClearColor(colorStr);
 }
 
 function animate() {
     animateBG();
-    updateMode();
+    updateGUI();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    // if (isSolved == 0) {
-    //     solve();
-    //     isSolved = 1;
-    // }
 }
 
 animate();
+displayGUI();
